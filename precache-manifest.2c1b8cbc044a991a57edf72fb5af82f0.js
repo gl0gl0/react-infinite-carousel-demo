d64ce000aba8accbb5830582b3482b959024f8ef
gl0gl0 <gloriana@leaf.fm>
2020-05-18 18:37:16 +0100

    Updates

---
+++ precache-manifest.2c1b8cbc044a991a57edf72fb5af82f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63d0a0cf010666da376dc2ad8b8b4ae",
+    "url": "/react-infinite-carousel-demo/index.html"
+  },
+  {
+    "revision": "abb28423ad193edd2ae8",
+    "url": "/react-infinite-carousel-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "463f465358cf7c57ff4a",
+    "url": "/react-infinite-carousel-demo/static/js/2.4f8374bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-infinite-carousel-demo/static/js/2.4f8374bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb28423ad193edd2ae8",
+    "url": "/react-infinite-carousel-demo/static/js/main.9f37a025.chunk.js"
+  },
+  {
+    "revision": "f22c5f38b47d6ecae6fa",
+    "url": "/react-infinite-carousel-demo/static/js/runtime-main.6f1a70a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-infinite-carousel-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
