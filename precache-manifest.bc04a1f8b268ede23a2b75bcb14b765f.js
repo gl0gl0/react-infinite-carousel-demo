5bd593388d10d1fe2300be5b35c1ba27fe587acd
gl0gl0 <gloriana@leaf.fm>
2020-05-18 19:49:11 +0100

    Updates

--- precache-manifest.bc04a1f8b268ede23a2b75bcb14b765f.js
+++ precache-manifest.bc04a1f8b268ede23a2b75bcb14b765f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26786d2c004968fbab69fad9cb8ad040",
+    "revision": "fcb16a545ce78ebe7e8b0bfc0f461ce4",
     "url": "/react-infinite-carousel-demo/index.html"
   },
   {
-    "revision": "6ba817a6e4599810972f",
+    "revision": "607e4807a45a5961c8ac",
     "url": "/react-infinite-carousel-demo/static/css/main.54a6a3e0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-infinite-carousel-demo/static/js/2.b45ec4c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6ba817a6e4599810972f",
-    "url": "/react-infinite-carousel-demo/static/js/main.3f591d0e.chunk.js"
+    "revision": "607e4807a45a5961c8ac",
+    "url": "/react-infinite-carousel-demo/static/js/main.40371c1e.chunk.js"
   },
   {
     "revision": "f22c5f38b47d6ecae6fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-infinite-carousel-demo/precache-manifest.2b79649c7786ce086ed4996f1d62575c.js"
+  "/react-infinite-carousel-demo/precache-manifest.bc04a1f8b268ede23a2b75bcb14b765f.js"
 );
 
 self.addEventListener('message', (event) => {
